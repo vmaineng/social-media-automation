f0953a839d087c9be32f117a002b1fd0a602f568
Mai Vang <vmaineng@gmail.com>
2025-03-30 17:03:48 -0700

    added in tests

--- app/store/themeStore.test.tsx
+++ app/store/themeStore.test.tsx
@@ -6,3 +6,24 @@ describe("themStore", () => {
     expect(theme).toBe("light");
   });
 });
+
+describe("toggleTheme", () => {
+  it("should change theme to dark when current is light", () => {
+    useThemeStore.getState().toggleTheme();
+    expect(useThemeStore.getState().theme).toBe("dark");
+  });
+  it("should change to light when current is dark", () => {
+    useThemeStore.getState().toggleTheme();
+    expect(useThemeStore.getState().theme).toBe("light");
+  });
+  it("should toggle correctly multiple times", () => {
+    const { toggleTheme } = useThemeStore.getState();
+
+    toggleTheme();
+    expect(useThemeStore.getState().theme).toBe("dark");
+    toggleTheme();
+    expect(useThemeStore.getState().theme).toBe("light");
+    toggleTheme();
+    expect(useThemeStore.getState().theme).toBe("dark");
+  });
+});
